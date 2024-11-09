aafb0472034216c34c22036f15b2b9b90e16bcc2
Rohit <rohit.dalvi2001@gmail.com>
2024-11-09 00:02:32 -0800

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // Apply COOP header with "same-origin" to all routes by default
+          source: "/:path*",
+          headers: [
+            {
+              key: "Cross-Origin-Opener-Policy",
+              value: "same-origin",
+            },
+          ],
+        },
+        {
+          // Apply COOP header with "unsafe-none" to /signin to prevent popup issues
+          source: "/",
+          headers: [
+            {
+              key: "Cross-Origin-Opener-Policy",
+              value: "unsafe-none",
+            },
+          ],
+        },
+        
+      ];
+    },
+    env: {
+        GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      },
+  };
+  
+  export default nextConfig;
+  
